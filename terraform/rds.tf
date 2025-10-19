578e56258a0b5caafc1ebe9dcd62d1ec36415bf2
Jaime <garciadiazjaime@gmail.com>
2025-10-19 15:23:24 -0500

    feat: adjust ip

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -35,14 +35,14 @@ resource "aws_security_group" "db_access" {
     from_port   = 3306
     to_port     = 3306
     protocol    = "tcp"
-    cidr_blocks = [var.my_ip] # Use the variable instead of hardcoded IP
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] # Allows outbound traffic to anywhere on the internet
   }
 
   tags = {
